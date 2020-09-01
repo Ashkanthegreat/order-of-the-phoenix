40f0cbe7508e0091c16af74173a7a1570f374896
Ashkan Abbasi <airashkan@yahoo.com>
2020-09-01 10:14:47 -0600

    Make test for search page

---
+++ spec/features/user_can_get_search_characters_by_house_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+feature 'user can search for members of a house' do
+  scenario 'user submits valid house name' do
+    visit '/'
+
+    select "Gryffindor", from: :house
+    click_on "Search For Members"
+    expect(current_path).to eq(search_path)
+  end
+end
